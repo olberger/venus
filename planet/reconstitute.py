b968e16eb657a7a682f641d2735113847f1c815a
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2014-01-22 13:17:28 +0100

    Adjust to an evolutions of html5lib

--- planet/reconstitute.py
+++ planet/reconstitute.py
@@ -17,7 +17,7 @@ import re, time, sgmllib
 from xml.sax.saxutils import escape
 from xml.dom import minidom, Node
 from html5lib import html5parser
-from html5lib.treebuilders import dom
+from html5lib import treebuilders
 import planet, config
 
 try:
@@ -168,7 +168,7 @@ def content(xentry, name, detail, bozo):
             bozo=1
 
     if detail.type.find('xhtml')<0 or bozo:
-        parser = html5parser.HTMLParser(tree=dom.TreeBuilder)
+        parser = html5parser.HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
         html = parser.parse(xdiv % detail.value, encoding="utf-8")
         for body in html.documentElement.childNodes:
             if body.nodeType != Node.ELEMENT_NODE: continue
